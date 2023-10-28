b8f4cb9d885ae5396787dcaf76763348f6eded88
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-10-28 08:49:54 +0500

    Storage Update Window Cereated

---
+++ src/Authorization.Desktop/Windows/Products/StorageUpdateWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Authorization.Desktop.Windows.Products
+{
+    /// <summary>
+    /// Interaction logic for StorageUpdateWindow.xaml
+    /// </summary>
+    public partial class StorageUpdateWindow : Window
+    {
+        public StorageUpdateWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
